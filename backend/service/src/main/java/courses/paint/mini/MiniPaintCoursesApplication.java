de8abb24720c999028789f10a369890a3ab268d8
MiSobecki <michal2000sob@gmail.com>
2022-10-04 13:08:51 +0100

    Initialize project and create backend structure.

---
+++ backend/service/src/main/java/courses/paint/mini/MiniPaintCoursesApplication.java
@@ -0,0 +1,13 @@
+package courses.paint.mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniPaintCoursesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniPaintCoursesApplication.class, args);
+    }
+
+}
