618eb6bd097f51b295191202e4e2d8e8a1bc7b81
MiSobecki <michal2000sob@gmail.com>
2022-10-04 13:49:48 +0100

    Implement connection to H2 database.

--- backend/service/src/main/java/courses/paint/mini/MiniPaintCoursesApplication.java
+++ backend/service/src/main/java/courses/paint/mini/MiniPaintCoursesApplication.java
@@ -2,8 +2,10 @@ package courses.paint.mini;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:mini-paint-courses-service.properties")
 public class MiniPaintCoursesApplication {
 
     public static void main(String[] args) {
