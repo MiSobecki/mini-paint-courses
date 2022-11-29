80e9c516eb96803f90e929639dc877344ec401ad
MiSobecki <michal2000sob@gmail.com>
2022-11-29 13:42:53 +0100

    Delete creating admin user in code. It should be done manually in db.

--- backend/service/src/main/java/courses/paint/mini/BasicRolesInitialization.java
+++ backend/service/src/main/java/courses/paint/mini/BasicRolesInitialization.java
@@ -1,39 +1,35 @@
 package courses.paint.mini;
 
+import courses.paint.mini.exception.user.NotFoundRoleException;
 import courses.paint.mini.model.Role;
-import courses.paint.mini.model.User;
-import courses.paint.mini.service.UserService;
 import courses.paint.mini.usecase.user.CreateRoleUseCase;
+import courses.paint.mini.usecase.user.GetRoleByNameUseCase;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.Set;
 
 @RequiredArgsConstructor
 @Component
 public class BasicRolesInitialization {
-
-    private final UserService userService;
     private final CreateRoleUseCase createRoleUseCase;
-
-    @Value("${admin.username}")
-    private String adminUsername;
-    @Value("${admin.password}")
-    private String adminPassword;
+    private final GetRoleByNameUseCase getRoleByNameUseCase;
 
     @PostConstruct
-    public void createBasicRolesAndAdminUser() {
-        var userRole = new Role(null, "USER");
-        userRole = createRoleUseCase.execute(userRole);
+    public void createBasicRoles() {
+        try {
+            getRoleByNameUseCase.execute("USER");
+        } catch (NotFoundRoleException ex) {
+            var userRole = new Role(null, "USER");
+            createRoleUseCase.execute(userRole);
+        }
 
-        var adminRole = new Role(null, "ADMIN");
-        adminRole = createRoleUseCase.execute(adminRole);
-
-        var adminUser = new User(null, adminUsername, adminPassword, Set.of(userRole, adminRole));
-
-        userService.register(adminUser);
+        try {
+            getRoleByNameUseCase.execute("ADMIN");
+        } catch (NotFoundRoleException ex) {
+            var adminRole = new Role(null, "ADMIN");
+            createRoleUseCase.execute(adminRole);
+        }
     }
 
 }
--- backend/service/src/test/java/courses/paint/mini/BasicRolesInitializationTest.java
+++ backend/service/src/test/java/courses/paint/mini/BasicRolesInitializationTest.java
@@ -1,40 +1,30 @@
 package courses.paint.mini;
 
+import courses.paint.mini.exception.user.NotFoundRoleException;
 import courses.paint.mini.model.Role;
-import courses.paint.mini.model.User;
-import courses.paint.mini.service.UserService;
 import courses.paint.mini.usecase.user.CreateRoleUseCase;
+import courses.paint.mini.usecase.user.GetRoleByNameUseCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BasicRolesInitializationTest {
-
-    @Mock
-    private UserService userService;
     @Mock
     private CreateRoleUseCase createRoleUseCase;
+    @Mock
+    private GetRoleByNameUseCase getRoleByNameUseCase;
 
     @InjectMocks
     private BasicRolesInitialization basicRolesInitialization;
 
-    @Captor
-    private ArgumentCaptor<User> userCaptor;
-
     @Test
-    public void shouldCreateBasicRolesAndAdminUserCorrectly() {
+    public void shouldCreateBasicRoles() {
         // given
         var userRole = new Role(null, "USER");
         doReturn(userRole).when(createRoleUseCase).execute(userRole);
@@ -42,15 +32,13 @@ public class BasicRolesInitializationTest {
         var adminRole = new Role(null, "ADMIN");
         doReturn(adminRole).when(createRoleUseCase).execute(adminRole);
 
+        doThrow(NotFoundRoleException.class).when(getRoleByNameUseCase).execute(anyString());
+
         // when
-        basicRolesInitialization.createBasicRolesAndAdminUser();
+        basicRolesInitialization.createBasicRoles();
 
         // then
         verify(createRoleUseCase, times(2)).execute(any(Role.class));
-
-        verify(userService).register(userCaptor.capture());
-        var user = userCaptor.getValue();
-        assertEquals(List.of("USER", "ADMIN"), user.getRoles().stream().map(Role::getName).collect(Collectors.toList()));
 
     }
 
