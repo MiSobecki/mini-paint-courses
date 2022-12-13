8dc38119f4a38a8c34f5095aedafc6e81d207490
MiSobecki <michal2000sob@gmail.com>
2022-12-13 12:42:53 +0100

    Implement homepage with list of courses.

--- frontend/src/app/main-content/homepage/homepage.component.ts
+++ frontend/src/app/main-content/homepage/homepage.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable, of} from "rxjs";
+import {CourseShortInfo} from "../../../shared/model/course-short-info";
+import {CourseService} from "../../../shared/service/course.service";
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+
+  courses: Observable<CourseShortInfo[]> = of([]);
+
+  constructor(private courseService: CourseService) {
+  }
+
+  ngOnInit(): void {
+    this.courses = this.courseService.coursesShortInfo$;
+
+    this.courseService.findAll();
+  }
 
 }
--- frontend/src/shared/service/course.service.ts
+++ frontend/src/shared/service/course.service.ts
@@ -7,7 +7,7 @@ import {environment} from "../../environments/environment";
 @Injectable({
   providedIn: 'root'
 })
-export class CourseServiceService {
+export class CourseService {
 
   private readonly ROOT_URL = '/api/courses';
 
@@ -15,13 +15,18 @@ export class CourseServiceService {
   coursesShortInfo$: Observable<CourseShortInfo[]> = this._coursesShortInfo.asObservable();
   private coursesShortInfoSub: Subscription | undefined;
 
+  totalPages = 0;
+
   constructor(private httpClient: HttpClient) {
   }
 
   findAll(): void {
-    this.coursesShortInfoSub = this.httpClient.get<CourseShortInfo[]>(environment.apiUrl + this.ROOT_URL).pipe(
-      tap((courses: CourseShortInfo[]) => {
+    this.coursesShortInfoSub = this.httpClient.get(environment.apiUrl + this.ROOT_URL).pipe(
+      tap((response: any) => {
+        const courses = response.content as CourseShortInfo[];
         this._coursesShortInfo.next(courses);
+
+        this.totalPages = response.totalPages;
       })
     ).subscribe();
   }
